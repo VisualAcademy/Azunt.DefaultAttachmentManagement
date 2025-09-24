ca6eb951fbb72c0963deec1ba2ed5fe10b624a3a
Park YongJune <redplus@redplus.net>
2025-09-24 14:01:03 +0900

    Add IDefaultAttachmentRepository interface with async CRUD and paging methods

---
+++ src/Azunt.DefaultAttachmentManagement/Azunt.DefaultAttachmentManagement/02_Contracts/IDefaultAttachmentRepository.cs
@@ -0,0 +1,13 @@
+﻿using Azunt.Models.Common;
+
+namespace Azunt.DefaultAttachmentManagement;
+
+public interface IDefaultAttachmentRepository
+{
+    Task<DefaultAttachment> AddAsync(DefaultAttachment model, string? connectionString = null);
+    Task<List<DefaultAttachment>> GetAllAsync(string? connectionString = null);
+    Task<DefaultAttachment> GetByIdAsync(long id, string? connectionString = null);
+    Task<bool> UpdateAsync(DefaultAttachment model, string? connectionString = null);
+    Task<bool> DeleteAsync(long id, string? connectionString = null);
+    Task<ArticleSet<DefaultAttachment, int>> GetAllAsync<TParentIdentifier>(int pageIndex, int pageSize, string searchField, string searchQuery, string sortOrder, TParentIdentifier parentIdentifier, string? connectionString = null);
+}
